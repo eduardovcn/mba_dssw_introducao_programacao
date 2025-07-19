fcfec80104930b275e7b18d594623bff82793eaf
eduardovitor <eduardovcn74@gmail.com>
2025-07-19 09:15:13 -0300

    Adjusting the impar_par challenge

--- impar_par.py
+++ impar_par.py
@@ -1,10 +1,10 @@
 numero = int(input("Qual numero você deseja verificar? "))
 
 
-if numero % 2 == 0:
-    print(F"O número {numero} é par")
+if (numero % 2) == 0:
+    print(f"O número {numero} é par")
 else:
-    print(F"O número {numero} é ímpar")
+    print(f"O número {numero} é ímpar")
 
 
     
